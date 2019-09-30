2aa00ff0346601410fe567eb82b0b9686f96e2c9
yulya <azizova3112@gmail.com>
2019-09-30 21:31:25 +0300

    initial commit

---
+++ app/src/main/java/com/azizova/loftmoney/MainActivity.java
@@ -0,0 +1,33 @@
+package com.azizova.loftmoney;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    public MainActivity(){
+        super();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
