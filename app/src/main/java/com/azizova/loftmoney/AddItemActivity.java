99c58e0342bea6f83fdc60b3ef983f6c8805764b
yulya <azizova3112@gmail.com>
2019-10-10 21:07:37 +0300

    Add window for listing items

--- app/src/main/java/com/azizova/loftmoney/AddItemActivity.java
+++ app/src/main/java/com/azizova/loftmoney/AddItemActivity.java
@@ -1,14 +1,22 @@
 package com.azizova.loftmoney;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AddItemActivity extends AppCompatActivity {
+    private EditText mNameEditText;
+    private EditText mPriceEditText;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
+
     }
 }
--- app/src/main/java/com/azizova/loftmoney/MainActivity.java
+++ app/src/main/java/com/azizova/loftmoney/MainActivity.java
@@ -1,10 +1,20 @@
 package com.azizova.loftmoney;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ItemsAdapter mAdapter;
 
     public MainActivity(){
         super();
@@ -29,5 +39,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.budget_item_list);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        mAdapter = new ItemsAdapter();
+        recyclerView.setAdapter(mAdapter);
+        mAdapter.addItem(new Item("Молоко", 70));
+        mAdapter.addItem(new Item("Зубная щётка", 70));
+        mAdapter.addItem(new Item("Новый телевизор", 20000));
+
     }
+
+
 }
