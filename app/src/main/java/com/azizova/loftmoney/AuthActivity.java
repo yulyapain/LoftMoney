79a6db73c1f09094728c134374632b3869f1cd38
yulya <azizova3112@gmail.com>
2019-10-20 16:32:55 +0300

    Added screen of authorization.

---
+++ app/src/main/java/com/azizova/loftmoney/AuthActivity.java
@@ -0,0 +1,63 @@
+package com.azizova.loftmoney;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceManager;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class AuthActivity extends AppCompatActivity {
+    private Api mApi;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acrivity_auth);
+
+        mApi = ((LoftApp)getApplication()).getApi();
+
+        Button authButton = findViewById(R.id.enter_button);
+        authButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                startActivity(new Intent(AuthActivity.this, MainActivity.class));
+            }
+        });
+
+        final String token = PreferenceManager.getDefaultSharedPreferences(this).getString(MainActivity.TOKEN, "");
+        if (TextUtils.isEmpty(token)){
+            auth();
+        }
+        else{
+            finish();
+            startActivity(new Intent(AuthActivity.this, MainActivity.class));
+        }
+    }
+
+    private void auth() {
+        String androidId = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
+        Call<Status> auth = mApi.auth(androidId);
+        auth.enqueue(new Callback<Status>() {
+            @Override
+            public void onResponse(Call<Status> call, Response<Status> response) {
+                SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(AuthActivity.this).edit();
+                editor.putString(MainActivity.TOKEN, response.body().getToken());
+                editor.apply();
+            }
+
+            @Override
+            public void onFailure(Call<Status> call, Throwable t) {
+            }
+        });
+    }
+}
--- app/src/main/java/com/azizova/loftmoney/MainActivity.java
+++ app/src/main/java/com/azizova/loftmoney/MainActivity.java
@@ -5,29 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
-import androidx.preference.PreferenceManager;
 import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-
 public class MainActivity extends AppCompatActivity {
 
     public static final String EXPENSE = "expense";
     public static final String INCOME = "income";
-    private static final String USER_ID = "u_azizova";
     public static final String TOKEN = "token";
-    private Api mApi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
 
                 Fragment activeFragment = getSupportFragmentManager().getFragments().get(activeFragmentIndex);
                 activeFragment.startActivityForResult(intent,BudgetFragment.REQUEST_CODE);
+                overridePendingTransition(R.anim.from_right_in, R.anim.from_left_out);
             }
         });
 
@@ -66,29 +58,11 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.getTabAt(0).setText(R.string.expences);
         tabLayout.getTabAt(1).setText(R.string.income);
 
-        mApi = ((LoftApp)getApplication()).getApi();
-        final String token = PreferenceManager.getDefaultSharedPreferences(this).getString(TOKEN, "");
-        if (TextUtils.isEmpty(token)) {
-            Call<Status> auth = mApi.auth(USER_ID);
-            auth.enqueue(new Callback<Status>() {
-                @Override
-                public void onResponse(Call<Status> call, Response<Status> response) {
-                    SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(MainActivity.this).edit();
-                    editor.putString(TOKEN, response.body().getToken());
-                    editor.apply();
-
-                    for (Fragment fragment : getSupportFragmentManager().getFragments()){
-                        if (fragment instanceof BudgetFragment){
-                            ((BudgetFragment) fragment).loadItems();
-                        }
-                    }
-                }
-
-                @Override
-                public void onFailure(Call<Status> call, Throwable t) {
-                }
-            });
+        for (Fragment fragment :getSupportFragmentManager().getFragments()) {
+            if (fragment instanceof BudgetFragment)
+                ((BudgetFragment) fragment).loadItems();
         }
+
     }
 
     static class BudgetPagerAdapter extends FragmentPagerAdapter{
